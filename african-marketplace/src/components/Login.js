ff1c05b41889846aeacd2a2cbe05e3cce9171b6e
Kristian Fulkerson <kristianf451@gmail.com>
2021-08-23 18:22:39 -0700

    cleaned up app.js, moved helper functions into signup/login components

--- african-marketplace/src/components/Login.js
+++ african-marketplace/src/components/Login.js
@@ -1,82 +1,91 @@
 import React, { useState } from "react";
-import axios from 'axios'
-import { useHistory } from "react-router";
+import { Link } from "react-router-dom";
 
-export default function Login(props) {
-    const {
-        values, 
-        submit, 
-        change, 
-        errors,
-    } = props 
+const initialFormValues = {
+  username: "",
+  password: ""
+};
+//initial error state
+const initialFormErrors = {
+  username: "",
+  password: ""
+};
 
-    const initialState = { // initial state for authorization
-        credentials: {
-            username: '',
-            password: '',
-        }
-    }
+const initialDisabled = true;
 
-    const [user, setUser] = useState(initialState)
-    const { push } = useHistory()
+export default function Login() {
+  //my code starts here
+  //state
+  const [loggedUsers, setLoggedUsers] = useState([]);
+  const [formValues, setFormValues] = useState(initialFormValues);
+  const [formErrors, setFormErrors] = useState(initialFormErrors);
+  const [disabled, setDisabled] = useState(initialDisabled);
 
-    const onSubmit = evt => {
-        evt.preventDefault()
-        submit()
-        axios.post('http://fakeapi.com', user.credentials)
-            .then(res=> {
-                localStorage.setItem("token", res.data.payload)
-                push('/item-list')
-            })
-            .catch(err=> {
-                console.log(err)
-            })
-    }
+  //function to detect change in input feilds
+  const inputChange = (name, value) => {
+    setFormValues({
+      ...formValues,
+      [name]: value
+    });
+  };
+  //POST new user
+  const postLoggedUser = logged => {
+    //eventual POST request using axios will go here
+    setLoggedUsers([logged, ...loggedUsers]);
+    setFormValues(initialFormValues);
+  };
+  //Sign up button submit
+  const login = () => {
+    const loggedUser = {
+      username: formValues.email.trim(),
+      password: formValues.password.trim()
+    };
+    postLoggedUser(loggedUser);
+  };
 
-    const onChange = evt => {
-        const { name } = evt.target
-        change(name)
-        setUser({
-            credentials: {
-                ...user.credentials,
-                [evt.target.name]: evt.target.value,
-            }
-        })
-    }
+  const onSubmit = e => {
+    e.preventDefault();
+    postLoggedUser();
+  };
+  const onChange = e => {
+    //works with checkboxes too if we add any
+    const { name, value } = e.target;
+    inputChange(name, value);
+  };
+  //my code ends here
 
   return (
     <form id="login-container" onSubmit={onSubmit}>
       <h1>Login Component</h1>
-
-      <div className='form-group login-inputs'>
-        <label>Username:
-            <input 
-                value={values.email}
-                onChange={onChange}
-                name='username'
-                type='text'
-            />
+      <div className="form-group login-submit">
+        <button id="login-button">Login</button>
+      </div>
+      <div className="form-group login-inputs">
+        <label>
+          Username:
+          <input
+            value={formValues.email}
+            onChange={onChange}
+            name="username"
+            type="email"
+          />
         </label>
 
-        <label>Password:
-            <input 
-                value={values.password}
-                onChange={onChange}
-                name='password'
-                type='text'
-            />
+        <label>
+          Password:
+          <input
+            value={formValues.password}
+            onChange={onChange}
+            name="password"
+            type="password"
+          />
         </label>
-      </div> 
+      </div>
 
-     <div className='form-group login-submit'>
-        <button id='login-button'>Login</button>
-     </div>
-
-        <div className='errors'>
-            <div>{errors.email}</div>
-            <div>{errors.password}</div>
-        </div>
-
+      <div className="errors">
+        <div>{formErrors.email}</div>
+        <div>{formErrors.password}</div>
+      </div>
     </form>
   );
 }
