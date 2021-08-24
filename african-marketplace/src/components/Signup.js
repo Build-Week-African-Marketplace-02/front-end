30d346d819fa54ab77d6974837dbcd9a2c6ddca5
cn8817 <christinenguyen1433@gmail.com>
2021-08-24 14:08:00 -0700

    styled signup and nav bar

--- african-marketplace/src/components/Signup.js
+++ african-marketplace/src/components/Signup.js
@@ -4,6 +4,59 @@ import { Link } from "react-router-dom";
 import SignupSchema from "../validation/signupSchema";
 import axios from "axios";
 
+import styled from "styled-components";
+
+const StyledSignup = styled.div`
+
+display:flex;
+justify-content:center;
+align-items:center;
+height:100vh;
+
+button{
+    display:flex;
+    justify-content:center;
+    border-radius:5px;
+    background-color:#ccccff;
+    }
+
+    button:hover{
+    background-color: grey;
+    color: white;
+    }
+
+    .signup{
+        padding:5rem;
+        height:50vh;
+        box-shadow:5px 10px 8px 10px #888888;
+    }
+
+    #signup-btn{
+        margin-top:20%;
+        margin-left:40%;
+        padding:3%;
+    }
+
+
+input, textarea:focus {
+  border: 5px solid #555;
+}
+
+  input, textarea{
+    display: block;
+    padding: .5rem .8rem .5rem .8rem;
+    margin: 5% 0;
+    border-radius: 5px;
+    font-size: 20px;
+    border: solid grey 2px;
+
+}
+
+  textarea{
+    height: 15vh;
+}
+`
+
 const initialFormValues = {
   name: "",
   password: "",
@@ -55,16 +108,16 @@ export default function Signup() {
   const postNewUser = newUser => {
     //eventual POST request using axios will go here
     axios
-      .post("https://african-marketplace-44.herokuapp.com/auth/api/login", newUser)
-      .then(res => {
-        setUsers([newUser, ...users]);
-      })
-      .catch(err => {
-        console.log(err);
-      })
-      .finally(() => {
-        setFormValues(initialFormValues);
-      })
+      .post("https://african-marketplace-44.herokuapp.com/api/auth/register", newUser)
+        .then(res => {
+          setUsers([res.data, ...users]);
+        })
+        .catch(err => {
+          console.log(err);
+        })
+        .finally(() => {
+          setFormValues(initialFormValues);
+        })
   };
   //Sign up button submit
   const signUpSubmit = () => {
@@ -77,7 +130,8 @@ export default function Signup() {
   };
   //side effects
   useEffect(() => {
-    SignupSchema.isValid(formValues).then(valid => setDisabled(!valid));
+    SignupSchema.isValid(formValues)
+      .then(valid => setDisabled(valid));
   }, [formValues]);
   const onSubmit = e => {
     e.preventDefault();
@@ -90,20 +144,18 @@ export default function Signup() {
     inputChange(name, valueToUse);
   };
   return (
+    <StyledSignup>
     <div className="signup container">
-      <h1>Sign Up Component</h1>
-      <Link to="/login">
+      <h1>Sign Up</h1>
+      {/* <Link to="/login">
         <button id="login-btn">Login!</button>
       </Link>
       <Link to="/">
         <button id="home-btn">Home</button>
-      </Link>
-      <h3>Sign Up:</h3>
+      </Link> */}
+      {/* <h3>Sign Up:</h3> */}
       <form id="signup-form" onSubmit={onSubmit}>
         <div className="form-group submit">
-          <button id="signup-btn" disabled={disabled}>
-            SIGN UP
-          </button>
           <div className="errors">
             <div>{formErrors.name}</div>
             <div>{formErrors.password}</div>
@@ -112,42 +164,40 @@ export default function Signup() {
         </div>
         <div className="form-group inputs">
           <h4>User Information:</h4>
-          <label>
-            Name:
             <input
               id="name-signup"
               type="text"
               value={formValues.name}
               onChange={onChange}
               name="name"
+              placeholder='username'
             />
-          </label>
-          <label>
-            Password:
             <input
               id="password-signup"
               type="password"
               value={formValues.password}
               onChange={onChange}
               name="password"
+              placeholder='password'
             />
-          </label>
-          <label>
-            Email:
             <input
               id="email-signup"
               type="email"
               value={formValues.email}
               onChange={onChange}
               name="email"
+              placeholder='email'
             />
-          </label>
         </div>
+        <button id="signup-btn" disabled={disabled}>
+            SIGN UP
+          </button>
       </form>
-      {users.map((user, index) => {
+      {/* {users.map((user, index) => {
         return <User key={index} details={user} />;
-      })}{" "}
+      })}{" "} */}
     </div>
+    </StyledSignup>
   );
 }
 
